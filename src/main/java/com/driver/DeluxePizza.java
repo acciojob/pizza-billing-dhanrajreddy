08b09e1863c6ad8accaf9da42bbc2de70d6ef2cd
dhanrajreddy <dhanrajkutwade7@gmail.com>
2023-12-20 01:37:07 +0530

    solved

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,10 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        if(!addCheese)
+            super.addExtraCheese();
+            super.addExtraToppings();
         // your code goes here
     }
 }
+
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,7 +1,8 @@
 package com.driver;
 
 public class Pizza {
-
+    boolean addCheese;
+    boolean addTapimg;
     private int price;
     private Boolean isVeg;
     private String bill;
@@ -9,6 +10,10 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg)
+            price+=300;
+        else
+            price+=400;
     }
 
     public int getPrice(){
@@ -17,18 +22,33 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+            if(!addCheese){
+                price+=80;
+                addCheese=true;
+            }
+               
     }
 
     public void addExtraToppings(){
         // your code goes here
+       if(!addTapimg){
+         if(isVeg)
+            price+=70;
+        else
+            price+=120;
+        addTapimg=true;
+       }
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        price+=20;
     }
 
     public String getBill(){
         // your code goes here
+        getPrice();
         return this.bill;
     }
 }
