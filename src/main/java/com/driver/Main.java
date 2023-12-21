61560c8f42dc67b641ba944e12b999f3b77e59eb
dhanrajreddy <dhanrajkutwade7@gmail.com>
2023-12-21 23:10:05 +0530

    seeAnsweredCode

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,31 +2,36 @@ package com.driver;
 
 public class Main {
   public static void main (String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
-    dp.addTakeaway();
-    dp.addExtraCheese();
-    dp.addTakeaway();
-    System.out.println(dp.getBill());
+//    DeluxePizza dp = new DeluxePizza(true);
+//    System.out.println(dp.getPrice());
+//    dp.addTakeaway();
+//    dp.addExtraCheese();
+//    dp.addTakeaway();
+//    System.out.println(dp.getBill());
+//
+//    Pizza p = new Pizza(false);
+//    p.addExtraCheese();
+//    p.addExtraToppings();
+//    p.addTakeaway();
+//    System.out.println(p.getBill());
 
-    Pizza p = new Pizza(false);
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addTakeaway();
-    System.out.println(p.getBill());
-
-//      Pizza p1 = new Pizza(true);
-//      System.out.println(p1.getPrice());//300
+      Pizza p1 = new Pizza(true);
+      p1.addExtraToppings();
+      p1.addExtraCheese();
+      p1.addTakeaway();
+      p1.addTakeaway();
+      p1.addExtraToppings();
+System.out.println(p1.getPrice());//300
 //      p1.addExtraToppings();//370
 //      System.out.println(p1.getPrice());//370
 //      p1.addTakeaway();//390
 //      p1.addExtraCheese();//470
 //      System.out.println(p1.getPrice());//470
-//
-//
-//    DeluxePizza dp1 = new DeluxePizza(true);
-//    //System.out.println("deluxPrice"+dp1.getPrice());
-//    System.out.println(dp1.getBill());
+
+
+    DeluxePizza dp1 = new DeluxePizza(true);
+    //System.out.println("deluxPrice"+dp1.getPrice());
+    System.out.println(dp1.getBill());
 
 
 
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,7 +1,7 @@
 package com.driver;
 import java.net.StandardSocketOptions;
 public class Pizza {
-    private int price, takeAwayPrice, cheesPrice, toppingPrice;
+    private int price, takeAwayPrice, cheesPrice, toppingPrice,basePrice;
     private boolean isVeg, takeAway, cheese, topping;
     private String bill;
 
@@ -10,11 +10,13 @@ public class Pizza {
         bill="";
         if (isVeg) {
             price = 300;
+            basePrice=300;
             takeAwayPrice = 20;
             cheesPrice = 80;
             toppingPrice = 70;
         } else {
             price = 400;
+            basePrice=400;
             takeAwayPrice = 20;
             cheesPrice = 80;
             toppingPrice = 120;
@@ -24,24 +26,30 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        if (!cheese)
+        if (!cheese){
             cheese= true;
+            price+=cheesPrice;
+        }
     }
     public void addExtraToppings(){
         // your code goes here
-        if (!topping)
+        if (!topping){
             topping= true;
+            price+=toppingPrice;
+        }
     }
-
     public void addTakeaway(){
         // your code goes here
-        if (! takeAway)
-            takeAway= true;
+        if (!takeAway){
+            takeAway=true;
+            price+=takeAwayPrice;
+        }
+
     }
 
 
     public String getBill() {
-            bill += "Base Price Of The Pizza: " + this.price+ "\n";
+            bill += "Base Price Of The Pizza: " + this.basePrice+ "\n";
         if (cheese)
             bill += "Extra Cheese Added: " + cheesPrice + "\n";
         if (topping)
@@ -49,20 +57,12 @@ public class Pizza {
 
         if (takeAway)
             bill += "Paperbag Added: " + (takeAwayPrice) + "\n";
-        bill += "Total Price: " + this.getPrice() + "\n";
+        bill += "Total Price: " +price +"\n";
         return bill;
     }
 
     public int getPrice()  {
-        int total=0;
-        if(cheese){
-           total += cheesPrice;
-        }
-        if(topping)
-            total += toppingPrice;
-        if (takeAway)
-            total += (takeAwayPrice);
-        return price+total;
+        return price;
     }
 }
 
