bbc3e8e64f036a623b74414f16b2280669e510f8
dhanrajreddy <dhanrajkutwade7@gmail.com>
2023-12-21 14:26:31 +0530

    seeCode

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,91 +1,34 @@
 package com.driver;
 
-import java.net.StandardSocketOptions;
-
 public class Pizza {
-    private int price, pacKageprice, sosPrice, toppingPrice, count;
-    private boolean isVej, pack, sos, topping;
+
+    private int price;
+    private Boolean isVeg;
     private String bill;
-    boolean isdeal;
 
-    public Pizza(boolean isVej) {
-        System.out.println("constructor called");
-        price = 0;
-        count = 0;
-        isdeal = false;
-        pack = false;
-        sos = false;
-        topping = false;
-        bill = "";
-        this.isVej = isVej;
-        if (isVej == true) {
-            price = 300;
-            pacKageprice = 20;
-            sosPrice = 80;
-            toppingPrice = 70;
-        } else {
-            price = 400;
-            pacKageprice = 20;
-            sosPrice = 80;
-            toppingPrice = 120;
-        }
-    }
-
-    public void methodPrice(){
-        if (isdeal)
-            price+=(sosPrice+toppingPrice);
-    }
-
-    public void addTakeaway() {
-        count++;
-        pack = true;
-    }
-
-    public void addExtraCheese() {
-        sos = true;
-
-    }
-
-    public void addExtraToppings() {
-        topping = true;
-
-    }
-
-    public void delPizzaCall() {
-        sos = true;
-        topping = true;
-    }
-
-    public String getBill() {
-        if (isdeal)
-            bill += "Base Price Of The Pizza: "+ price +"\n";
-        else
-            bill += "Base Price Of The Pizza: " + this.price+ "\n";
-        if (sos)
-            bill += "Extra Cheese Added: " + sosPrice + "\n";
-        if (topping)
-            bill += "Extra Toppings Added: " + toppingPrice + "\n";
-
-        if (pack)
-            bill += "Paperbag Added: " + (count * pacKageprice) + "\n";
-        bill += "Total Price: " + this.getPriceCount() + "\n";
-        return bill;
+    public Pizza(Boolean isVeg){
+        this.isVeg = isVeg;
+        // your code goes here
     }
 
     public int getPrice(){
-        System.out.print("Base Price Of The Pizza: ");
-        return price;
+        return this.price;
     }
 
-    public int getPriceCount() {
-        int total=0;
-        if(sos && !isdeal){
-           total += sosPrice;
-        }
-        if(topping  && !isdeal)
-            total += toppingPrice;
-        if (pack)
-            total += (count * pacKageprice);
-        return price+total;
+    public void addExtraCheese(){
+        // your code goes here
+    }
+
+    public void addExtraToppings(){
+        // your code goes here
+    }
+
+    public void addTakeaway(){
+        // your code goes here
+    }
+
+    public String getBill(){
+        // your code goes here
+        return this.bill;
     }
 }
